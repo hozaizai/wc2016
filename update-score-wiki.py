e0fe7eac96bdc213c62243cce0565fc001fcc547
hozaizai <96908372+hozaizai@users.noreply.github.com>
2026-06-15 10:51:35 +0800

    Update URL in update-score-wiki.py

--- update-score-wiki.py
+++ update-score-wiki.py
@@ -3,7 +3,7 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-url = "https://script.google.com/macros/s/AKfycbybMkJ1-1aLxXWaMtBltI3Qpf7Va4cVkdrCNxzM-HRXwcOIQ9QjDSDPxqmDtwv2D1A3/exec"
+url = "https://script.google.com/macros/s/AKfycbyV15RT-DWy_cjlvYPJK-eO67wmcrOKs0DrtO28esuqIldH1ghtRxpD_YDtEC31TYl1eA/exec"
 
 response = requests.get(url, allow_redirects=True)
 with open("data-score-wiki.json", "w", encoding="utf-8") as f:
